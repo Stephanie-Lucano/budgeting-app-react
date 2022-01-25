92da8a99b6a31d32d20585c7f4e2263188518a70
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-25 05:13:31 -0500

    Moved the logo from the Nav to Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,11 @@
+import Logo from "../Assests/Budget.png"
 const Home = () => {
     return (
         <div className="Home">
             <h1>Home</h1>
+            <h2>Welcome to the Budget App</h2>
+            <img src={Logo} alt="Person tracking expenses" width="400" height="300" />
+            
         </div>
     )
 }
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,12 +1,9 @@
 import { Link } from "react-router-dom"
-import Logo from "../Assests/Budget.png"
 
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <Link to="/">
-                <img src={Logo} alt="Budgeting" width="200" height="150" />
-            </Link>
+            <Link to="/"><button>Budget App</button></Link>
             <Link to="/transactions"><button>Index</button></Link>
             <Link to="/transactions/new"><button>New</button></Link>
             
