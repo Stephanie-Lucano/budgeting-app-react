226fa4c38ea2fb3a431d5046e423c1de6acba437
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-25 01:25:06 -0500

    Type name to source

--- src/Functionality/SingleTransaction.js
+++ src/Functionality/SingleTransaction.js
@@ -4,7 +4,7 @@ const SingleTransaction = ({transaction, index}) => {
     return (
         <tr className="SingleTransaction">
             <td>{transaction.date}</td>
-            <td>{transaction.name}</td>
+            <td>{transaction.source}</td>
             <td>
                 <Link to={`/transactions/${index}`}>
                     {transaction.amount}
