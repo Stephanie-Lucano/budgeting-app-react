18fc7bf79f2a0f755f52fbc464f300146622538e
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-01-25 02:26:43 -0500

    Render Show page

--- src/Functionality/ShowTransaction.js
+++ src/Functionality/ShowTransaction.js
@@ -1,6 +1,38 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
 const ShowTransaction = () => {
+    const navigate = useNavigate()
+    const URL = process.env.REACT_APP_API_URL
+    const { id } = useParams()
+    const [transaction, setTransaction] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`${URL}/transactions/${id}`)
+            .then((response) => {
+                setTransaction(response.data)
+            })
+    }, [])
+
+    const handleDelete = (event) => {
+        axios
+            .delete(`${URL}/transactions/${id}`)
+            .then(() => {
+                navigate("/transactions")
+            })
+    }
+
     return (
         <div className="ShowTransaction">
+            <article>
+                <h1>{transaction.date}</h1>
+                <h2>{transaction.source}</h2>
+                <h2>{"$"+ transaction.amount}</h2>
+                <h2>{transaction.from}</h2>
+                <h2>{transaction.category}</h2>
+            </article>
         </div>
     )
 }
